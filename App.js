e8822f5dbf7702dfb54b65d32b37b4abaa265670
joshrs1199 <46760607+joshrs1199@users.noreply.github.com>
2019-10-14 22:40:53 -0400

    updated searchbar switched the state over to searchbar component

--- App.js
+++ App.js
@@ -2,31 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SearchBar from './SearchBar'
 
-class App extends React.Component {
+function App() {
 
-  state = {
-    search: '',
-    apiKey: 'i9ILNojyV0rEfrbzBuuPZxi6GnlLd-Cf4x7et90cMeTO5WpkO6cyJLpW1TcXnDwUBCgnnjR6yiyNxGwyxHvNoOH3C7QdjlXNi5QD8h6sxUIHJ3n5r3RHvVlAjiGlXXYx'
-  };
-
-  handleSearch = (event) => {
-    this.setState({
-      search: event
-    })
-  }
-
-  handleSubmit = () => {
-    console.log(this.state.apiKey)
-  }
-
-  render() {
     return (
       <View style={styles.container}>
-        <SearchBar searchSubmit={this.handleSubmit} searchState={this.state.search} handleSearch={this.handleSearch}/>
+        <SearchBar />
       </View>
     );
   };
-}
 
 const styles = StyleSheet.create({
   container: {
--- SearchBar.js
+++ SearchBar.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { Button, TextInput, View } from 'react-native';
 
-function SearchBar(props) {
-  return (
-    <View>
-      <Button title='Search' onPress={props.searchSubmit}/>
-      <TextInput name='term' onChangeText={props.handleSearch} placeholder='Term' value={props.searchState}/>
-    </View>
-  )
-}
+class SearchBar extends React.Component {
+
+  state = {
+    term: '',
+    location: '',
+    apiKey: 'i9ILNojyV0rEfrbzBuuPZxi6GnlLd-Cf4x7et90cMeTO5WpkO6cyJLpW1TcXnDwUBCgnnjR6yiyNxGwyxHvNoOH3C7QdjlXNi5QD8h6sxUIHJ3n5r3RHvVlAjiGlXXYx'
+  }
+
+  handleChange = (event) => {
+    console.log(event.target)
+    // this.setState({
+    //   [event.target.name]: event.target.value
+    // })
+  }
+
+  handleSubmit = () => {
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <View>
+        <Button title='Search' onPress={this.handleSubmit}/>
+        <TextInput name='term' onChangeText={this.handleChange} placeholder='term' value={this.state.term}/>
+        <TextInput name='location' onChangeText={this.handleChange} placeholder='location' value={this.state.location}/>
+      </View>
+    );
+  };
+};
 export default SearchBar;
